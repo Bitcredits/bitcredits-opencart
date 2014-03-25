7e602d44b9a2feba5f22c5aba8cba40319ee87a3
Alexandre Bourget <alex@bourget.cc>
2014-03-24 21:19:19 -0400

    Cosmetics

--- admin/language/english/payment/bitcredits.php
+++ admin/language/english/payment/bitcredits.php
@@ -4,7 +4,7 @@ $_['heading_title']      = 'Bitcredits';
 
 // Text
 $_['text_payment']       = 'Payment';
-$_['text_success']       = 'Success: You have modified the bitcredits payment module!';
+$_['text_success']       = 'Success: You have modified the Bitcredits payment module!';
 $_['text_bitcredits'] = '<a onclick="window.open(\'http://www.bitcredits.io/\');"><img src="view/image/payment/bitcredits.png" alt="bitcredits" title="bitcredits" style="border: 0px solid #EEEEEE;" /></a>';
 
 // Entry
